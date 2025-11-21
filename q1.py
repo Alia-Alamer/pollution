f14d836eaf5ed8234b7a505afc87ca8a58a91879
Alia Alamer <alia.alamer@edu.fh-joanneum.at>
2025-11-21 22:21:03 +0100

    analyze the relationship between PM10/NO2 and predictors

--- q1.py
+++ q1.py
@@ -8,27 +8,27 @@ with open ('feinstaubdataexercise.pickle', 'rb') as file:
     filecontent = pkl.load(file)
 
 #print(filecontent)
-data = filecontent["Graz-DB"]
+content = filecontent["Graz-DB"]
 #print(data.head())
 
-print("dataset shape:", data.shape)
-print("\n spalten:", data.columns.tolist())
-print(f"von {data.index.min()} bis {data.index.max()}")
+print("dataset shape:", content.shape)
+print("\n spalten:", content.columns.tolist())
+print(f"von {content.index.min()} bis {content.index.max()}")
 
 print("=========================")
 
 print("\n statistiken der response variablen:")
-print(data[['pm10', 'no2']].describe())
+print(content[['pm10', 'no2']].describe())
 
 fig, axes = plt.subplots(2, 1, figsize = (15, 10))
 
-axes[0].plot(data.index, data['pm10'], alpha=0.7)
+axes[0].plot(content.index, content['pm10'], alpha=0.7)
 axes[0].set_title('pm10 Konzentration über Zeit')
 axes[0].set_ylabel('pm10 (μg/m³)')
 #axes[0].set_xlabel('Datum')
 axes[0].grid(True)
 
-axes[1].plot(data.index, data['no2'], alpha=0.7, color='pink')
+axes[1].plot(content.index, content['no2'], alpha=0.7, color='pink')
 axes[1].set_title('no2 Konzentration über Zeit')
 axes[1].set_ylabel('no2 (μg/m³)')
 axes[1].set_xlabel('Datum')
@@ -36,3 +36,18 @@ axes[1].grid(True)
 
 plt.tight_layout()
 plt.show()
+
+
+predictors = ['temp', 'humidity', 'windspeed', 'prec', 'peak_velocity']
+
+for preds in predictors:
+    plt.figure(figsize=(6,4))
+    sns.scatterplot(data=content, x=preds, y="pm10")
+    plt.title(f"pm10 vs {preds}")
+    plt.show()
+
+    plt.figure(figsize=(6,4))
+    sns.scatterplot(data=content, x=preds, y="no2")
+    plt.title(f"no2 vs {preds}")
+    plt.show()
+
