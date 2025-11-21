2c146cfcb75507a7a5412b341315892ed08512e8
Alia Alamer <alia.alamer@edu.fh-joanneum.at>
2025-11-21 19:50:23 +0100

    added visualization for the time resolved pollutant data

--- q1.py
+++ q1.py
@@ -19,3 +19,20 @@ print("=========================")
 
 print("\n statistiken der response variablen:")
 print(data[['pm10', 'no2']].describe())
+
+fig, axes = plt.subplots(2, 1, figsize = (15, 10))
+
+axes[0].plot(data.index, data['pm10'], alpha=0.7)
+axes[0].set_title('pm10 Konzentration über Zeit')
+axes[0].set_ylabel('pm10 (μg/m³)')
+#axes[0].set_xlabel('Datum')
+axes[0].grid(True)
+
+axes[1].plot(data.index, data['no2'], alpha=0.7, color='pink')
+axes[1].set_title('no2 Konzentration über Zeit')
+axes[1].set_ylabel('no2 (μg/m³)')
+axes[1].set_xlabel('Datum')
+axes[1].grid(True)
+
+plt.tight_layout()
+plt.show()
