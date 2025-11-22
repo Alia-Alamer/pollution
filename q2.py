0a2c34b4715cda5f119095f8ce1e677fc10e8fd1
Alia Alamer <alia.alamer@edu.fh-joanneum.at>
2025-11-22 02:38:51 +0100

    added solution for second task

---
+++ q2.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+from statsmodels.stats.anova import anova_lm
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle as pkl
+
+
+with open ('feinstaubdataexercise.pickle', 'rb') as file:
+    filecontent = pkl.load(file)
+
+
+content = filecontent["Graz-DB"]
+
+model_pm10 = smf.ols(formula="pm10 ~ C(day_type) + humidity + temp + prec + windspeed + peak_velocity", data=content).fit()
+print(model_pm10.summary())
+
+anova_lm(model_pm10)
+
+
+
+model_no2 = smf.ols(formula="no2 ~ C(day_type) + humidity + temp + prec + windspeed + peak_velocity", data=content).fit()
+print(model_no2.summary())
+
+anova_lm(model_no2)
+
+
+
+
+sns.scatterplot(x=model_pm10.fittedvalues, y=model_pm10.resid)
+plt.axhline(0, color="red")
+plt.title("pm10 residuen vs. fitted")
+plt.show()
+
+
+sns.scatterplot(x=model_no2.fittedvalues, y=model_no2.resid)
+plt.axhline(0, color="red")
+plt.title("no2 residuen vs. fitted")
+plt.show()
+
+
+sm.qqplot(model_pm10.resid, line='s')
+plt.title("PM10 QQ-Plot")
+plt.show()
+
+sm.qqplot(model_no2.resid, line='s')
+plt.title("NO2 QQ-Plot")
+plt.show()
